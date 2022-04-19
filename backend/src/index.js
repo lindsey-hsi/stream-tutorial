8520f6d87f7abf9eb8b9776258acfcf30b0d3052
Lindsey Hsi <lhsi@uchicago.edu>
2022-04-19 14:08:06 -0500

    another try

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,8 @@ const api = express();
 
 // api.use(cors());
 api.use(cors({
-    origin: 'https://celadon-llama-ce8ee1.netlify.app'
+    origin: 'https://celadon-llama-ce8ee1.netlify.app',
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
 }));
 api.use(compression());
 api.use(helmet());
