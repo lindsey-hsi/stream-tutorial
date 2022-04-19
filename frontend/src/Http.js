565ca8da03b48ba5bf7e2a853da68aa82ecc3514
Lindsey Hsi <lhsi@uchicago.edu>
2022-04-19 15:06:37 -0500

    modified header

--- frontend/src/Http.js
+++ frontend/src/Http.js
@@ -2,6 +2,10 @@ export const post = (url, body, maybeAuth) => {
   let headers = {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': 'https://celadon-llama-ce8ee1.netlify.app',
+    'Access-Control-Allow-Methods': 'POST,GET',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Max-Age': '86400',
   };
 
   if (maybeAuth) {
