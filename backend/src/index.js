db31a4ef680aaab8e421b817dd300a7b21195fdb
Lindsey Hsi <lhsi@uchicago.edu>
2022-04-20 10:33:46 -0500

    trying corsOptions again

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,14 +11,14 @@ dotenv.config();
 
 var api = express();
 
-// var corsOptions = {
-//   origin: 'https://celadon-llama-ce8ee1.netlify.app',
-//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// }
-app.use(cors())
-app.get('https://celadon-llama-ce8ee1.netlify.app', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for only example.com.'})
-})
+var corsOptions = {
+  origin: 'https://celadon-llama-ce8ee1.netlify.app',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+// app.use(cors())
+// app.get('https://celadon-llama-ce8ee1.netlify.app', function (req, res, next) {
+//   res.json({msg: 'This is CORS-enabled for only example.com.'})
+// })
 // api.get("/no-cors", (req, res) => {
 //   console.info("GET /no-cors");
 //   res.json({
@@ -26,7 +26,7 @@ app.get('https://celadon-llama-ce8ee1.netlify.app', function (req, res, next) {
 //   });
 // });
 // api.options('https://celadon-llama-ce8ee1.netlify.app/v1/authenticate', cors())
-// api.use(cors(corsOptions));
+api.use(cors(corsOptions));
 // api.use(cors({
 //     origin: 'https://celadon-llama-ce8ee1.netlify.app',
 //     methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
