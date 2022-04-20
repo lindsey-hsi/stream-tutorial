034ae6b166e14dd7819c2b851becfd5bbaccad7a
Lindsey Hsi <lhsi@uchicago.edu>
2022-04-20 09:46:46 -0500

    adding to header in http.js

--- frontend/src/Http.js
+++ frontend/src/Http.js
@@ -2,8 +2,8 @@ export const post = (url, body, maybeAuth) => {
   let headers = {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
-    // 'Access-Control-Allow-Origin': 'https://celadon-llama-ce8ee1.netlify.app',
-    // 'Access-Control-Allow-Methods': 'POST,GET',
+    'Access-Control-Allow-Origin': 'https://celadon-llama-ce8ee1.netlify.app',
+    'Access-Control-Allow-Methods': 'POST,GET',
     // 'Access-Control-Allow-Headers': 'Content-Type',
     // 'Access-Control-Max-Age': '86400',
   };
