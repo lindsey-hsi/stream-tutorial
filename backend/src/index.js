b17cd104837ec03012da5de70264a324b3e7fe0e
Lindsey Hsi <lhsi@uchicago.edu>
2022-04-21 11:51:49 -0500

    some cleanup before switching to main

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,36 +10,9 @@ import compression from 'compression';
 dotenv.config();
 
 var api = express();
-// var headers = {
-//   'Accept': 'application/json',
-//   'Content-Type': 'application/json',
-//   'Access-Control-Allow-Origin': 'https://celadon-llama-ce8ee1.netlify.app',
-//   'Access-Control-Allow-Methods': 'POST,GET',
-//   // 'Access-Control-Allow-Headers': 'Content-Type',
-//   // 'Access-Control-Max-Age': '86400',
-// };
 
-// var corsOptions = {
-//   origin: 'https://celadon-llama-ce8ee1.netlify.app',
-//   headers: headers,
-//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// }
-app.use(cors())
-// app.get('https://celadon-llama-ce8ee1.netlify.app', function (req, res, next) {
-//   res.json({msg: 'This is CORS-enabled for only example.com.'})
-// })
-// api.get("/no-cors", (req, res) => {
-//   console.info("GET /no-cors");
-//   res.json({
-//     text: "You should not see this via a CORS request."
-//   });
-// });
-// api.options('https://celadon-llama-ce8ee1.netlify.app/v1/authenticate', cors())
-// api.use(cors(corsOptions));
-// api.use(cors({
-//     origin: 'https://celadon-llama-ce8ee1.netlify.app',
-//     methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
-// }));
+api.use(cors())
+
 api.use(compression());
 api.use(helmet());
 api.use(bodyParser.urlencoded({ extended: true }));
--- frontend/src/Http.js
+++ frontend/src/Http.js
@@ -2,27 +2,14 @@ export const post = (url, body, maybeAuth) => {
   let headers = {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
-    // 'Access-Control-Allow-Origin': 'https://celadon-llama-ce8ee1.netlify.app',
-    // 'Access-Control-Allow-Methods': 'POST,GET',
-    // 'Access-Control-Allow-Headers': 'Content-Type',
-    // 'Access-Control-Max-Age': '86400',
+
   };
 
   if (maybeAuth) {
     headers['Authorization'] = 'Bearer ' + maybeAuth;
   }
 
-  // const goFetch = fetch(url, {
-  //   method: "POST",
-  //   headers: headers,
-  //   body: JSON.stringify(body),
-  // })
-  //
-  // console.info(goFetch)
-
-  // return goFetch.then(res => res.json())
     return fetch(url, {
-      // mode: 'no-cors',
       method: "POST",
       headers: headers,
       body: JSON.stringify(body),
